4c889c0718983db9a2c43aa4945675217ede3ce5
Diffi <diffi.penn@gmail.com>
2024-05-19 23:19:10 +0000

    prod changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Test Page</h1>
+<p>Visit <a href="./map">The Map</a></p>
+<p>Visit <a href="./map-test">The Test Area</a></p>
 
 <style>
 	h1 {
