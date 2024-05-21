c04fc21d492ff78e80b706b43ce446aea2a87369
TheDiffi <64838662+TheDiffi@users.noreply.github.com>
2024-05-21 21:19:35 +0200

    Adjusted landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
-<h1>Test Page</h1>
-<p>Visit <a href="./map">The Map</a></p>
-<p>Visit <a href="./map-test">The Test Area</a></p>
-
+<div style="margin: 1em; padding-top:2em;text-align: center;">
+	<h1>Test Page</h1>
+	<p>Visit <a href="./map">The Map</a></p>
+	<p>Visit <a href="./map-test">The Test Area</a></p>
+</div>
 
 <style>
 	h1 {
 		color: #ff3e00;
+		font-size: 5em;
+		font-synthesis: 1px;
+		padding-bottom: 0;
 	}
 	p {
 		color: #333;
